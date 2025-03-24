90cde548bd5dda9931eb059b91abcd0047c8f759
Mraanderson <ajaxupgrades@gmail.com>
2025-03-24 22:27:05 +0000

    Update shutdown.py

--- Shutdown.py
+++ Shutdown.py
@@ -2,10 +2,8 @@
 from gpiozero import Button
 import os
 
-#Button 17 = Pin 11+Gnd
-#Button 21 = Pin 40+Gnd
 #Button 3 = Pin 5+Gnd (also acts as a reset button to bring up again)
 
 Button(3).wait_for_press()
-#os.system("sudo poweroff")
-os.system("sudo reboot")
+os.system("sudo poweroff")
+#os.system("sudo reboot")
